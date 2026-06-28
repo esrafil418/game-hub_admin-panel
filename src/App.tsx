1d95cac2cf38b5bc2f80d39f26c40af0c83e7cb4
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-29 00:33:26 +0330

    feat: Add Sidebar Options

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import Add from "./page/add/Add";
+import List from "./page/list/List";
+import Orders from "./page/orders/Orders";
 
 export default function App() {
 	return (
@@ -8,6 +12,11 @@ export default function App() {
 			<hr />
 			<div className="flex">
 				<Sidebar />
+				<Routes>
+					<Route path="/add" element={<Add />} />
+					<Route path="/list" element={<List />} />
+					<Route path="/orders" element={<Orders />} />
+				</Routes>
 			</div>
 		</div>
 	);
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,3 +1,46 @@
+import { CircleCheckBig, CirclePlus } from "lucide-react";
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar() {
-	return <div>Sidebar</div>;
+	return (
+		<div className="w-[18%] min-h-screen border border-[#a9a9a9] border-t-0">
+			<div className="pt-10 md:pt-12.5 pl-4 md:pl-[20%] flex flex-col gap-5">
+				<NavLink
+					to="/add"
+					className={({ isActive }) =>
+						`flex items-center gap-3 border border-[#a9a9a9] border-r-0 px-2.5 py-2 rounded-l cursor-pointer transition-colors duration-200 ${
+							isActive ? "bg-[#fff0ed] border-turquoise" : "hover:bg-gray-50"
+						}`
+					}
+				>
+					<CirclePlus />
+					<p className="hidden sm:block">Add Items</p>
+				</NavLink>
+
+				<NavLink
+					to="/list"
+					className={({ isActive }) =>
+						`flex items-center gap-3 border border-[#a9a9a9] border-r-0 px-2.5 py-2 rounded-l cursor-pointer transition-colors duration-200 ${
+							isActive ? "bg-[#fff0ed] border-turquoise" : "hover:bg-gray-50"
+						}`
+					}
+				>
+					<CircleCheckBig />
+					<p className="hidden sm:block">List Items</p>
+				</NavLink>
+
+				<NavLink
+					to="/orders"
+					className={({ isActive }) =>
+						`flex items-center gap-3 border border-[#a9a9a9] border-r-0 px-2.5 py-2 rounded-l cursor-pointer transition-colors duration-200 ${
+							isActive ? "bg-[#fff0ed] border-turquoise" : "hover:bg-gray-50"
+						}`
+					}
+				>
+					<CircleCheckBig />
+					<p className="hidden sm:block">Orders</p>
+				</NavLink>
+			</div>
+		</div>
+	);
 }
